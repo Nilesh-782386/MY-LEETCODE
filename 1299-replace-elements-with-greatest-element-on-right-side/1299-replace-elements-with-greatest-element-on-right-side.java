88e3a7227b94b17d5413d8e13e06ac62013285a2
Nilesh Khedkar <khedkarnilesh28@gmail.com>
2026-08-01 16:56:23 +0530

    Time: 2 ms (99.54%), Space: 49.4 MB (17.29%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-
      int ans[] = new int[arr.length];
-     Stack<Integer>s  = new Stack<>();
-     s.push(arr[arr.length-1]);
+    int  curr = arr[arr.length-1];
      ans[arr.length-1] = -1;
      for( int i = arr.length-2;i>=0 ;i--){
 
-if(s.size() > 0 && arr[i] > s.peek()){
-    ans[i] = s.pop();
-    s.push(arr[i]);
-}else{
-    ans[i] = s.peek(); 
+
+    // ans[i] = curr;
+
+   if(arr[i] > curr ) {
+    ans[i] = curr;
+    curr = arr[i];
+   }else{
+    ans[i] = curr;
 }
      }
 
